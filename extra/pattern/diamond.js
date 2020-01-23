150edb90ecfe4b55cace3de304d32a45ec19491c
SDmayank <mayank.garg@successive.tech>
2020-01-23 17:59:16 +0530

    feat: Diamond pattern(#39515)

---
+++ extra/pattern/diamond.js
@@ -0,0 +1,34 @@
+var rows = Number(process.argv[2]);
+function Diamond(rows)
+{
+    string = "";
+    for(i=1; i<=rows;i++)
+    {
+        for(space=rows-i; space>0; space--)
+        {
+            string = string + " "
+        }
+        for(star = 1; star<=i; star++)
+        {
+          string=string + "* " 
+        }
+        string+= "\n"
+        
+    }
+    for(i=1; i<=rows;i++)
+    {
+        for(space = 1; space <= i; space++)
+        {
+          string=string + " "
+        }
+        for(star = rows-i; star>0; star--)
+        {
+            string=string + "* "  
+        }
+        string+= "\n"
+        
+    }
+
+console.log(string)
+}
+Diamond(rows)
