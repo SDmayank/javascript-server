798d4da525bea527e490eef53c94341a1b5c927a
SDmayank <mayank.garg@successive.tech>
2020-02-11 12:19:49 +0530

    feat:add profile setup/#40012

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -3,11 +3,17 @@ import { default as UserController } from './Controller';
 import validationHandler from '../../libs/routes/validationHandler';
 import { validation } from './validation';
 import authMiddleware from '../../libs/authMiddleware';
-const userRouter = Router();
+import IRequest from '../../libs/routes/IRequest';
 
+const userRouter = Router();
 userRouter.route('/')
-    .get(authMiddleware('getUsers', 'read'), validationHandler(validation.get), UserController.create)
-    .post(authMiddleware('getUsers', 'read'), validationHandler(validation.create), UserController.list)
-    .put(validationHandler(validation.update), UserController.update);
-userRouter.delete('/:id', authMiddleware('getUsers', 'read'), validationHandler(validation.delete), UserController.delete);
+  .get(authMiddleware('getUsers', 'read'), validationHandler(validation.get), UserController.create)
+  .post(authMiddleware('getUsers', 'read'), validationHandler(validation.create), UserController.create)
+  .put(authMiddleware('getUsers', 'read'), validationHandler(validation.update), UserController.update);
+userRouter.delete('/:id', validationHandler(validation.delete), UserController.delete);
+userRouter.route('/me')
+  .get(authMiddleware('getUsers', 'read'), (req: IRequest, res) => {
+    console.log('Inside routes', req.user);
+    res.send(req.user);
+  });
 export default userRouter;
